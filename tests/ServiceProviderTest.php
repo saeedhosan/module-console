1ee66c4fd3f08d646e787fe0e470806eb1bb3b4e
Saeed Hosan <saeedhosansh@gmail.com>
2026-04-12 00:52:52 +0600

    test: add test suite for service provider

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Artisan;
+use SaeedHosan\Module\Console\ServiceProvider;
+use Tests\TestCase;
+
+uses(TestCase::class);
+
+it('registers migration creator alias', function () {
+    $app      = app();
+    $provider = new ServiceProvider($app);
+    $provider->register();
+
+    expect($app->bound('migration.creator'))->toBeTrue();
+});
+
+it('publishes stubs when running in console', function () {
+    $app      = app();
+    $provider = new ServiceProvider($app);
+    $provider->boot();
+
+    $publishes = (new ReflectionClass($provider))->getProperty('publishes');
+
+    expect($publishes->getValue($provider))->not->toBeEmpty();
+});
+
+it('registers generate module skeleton command', function () {
+    $app      = app();
+    $provider = new ServiceProvider($app);
+    $provider->boot();
+
+    expect($provider)->toBeInstanceOf(ServiceProvider::class);
+});
+
+it('registers module list command', function () {
+    $app = app();
+
+    Artisan::call('list');
+
+    expect(Artisan::output())->toContain('module');
+});
+
+test('returns available make commands', function () {
+    $reflection = new ReflectionClass(ServiceProvider::class);
+    $method     = $reflection->getMethod('availableMakeCommands');
+
+    $result = $method->invoke(new ServiceProvider(app()));
+
+    expect($result)->toBeArray();
+    expect($result)->not->toBeEmpty();
+    expect($result)->toContain(Illuminate\Foundation\Console\CastMakeCommand::class);
+});
+
+test('overrides make commands using WithModuleCommand', function () {
+    $reflection = new ReflectionClass(ServiceProvider::class);
+    $method     = $reflection->getMethod('moduleCommandClass');
+
+    $provider = new ServiceProvider(app());
+    $result   = $method->invoke($provider, Illuminate\Foundation\Console\CastMakeCommand::class);
+
+    expect($result)->toBeString();
+    expect($result)->toStartWith('Module_');
+    expect(class_exists($result))->toBeTrue();
+
+    $traits = class_uses($result);
+
+    expect($traits)->toHaveKey(SaeedHosan\Module\Console\Concerns\WithModuleCommand::class);
+});
+
+it('overrides make commands should have module option', function () {
+
+    $makeCommands = [
+        'make:cast',
+        'make:channel',
+        'make:class',
+        'make:component',
+        'make:config',
+        'make:command',
+        'make:enum',
+        'make:event',
+        'make:exception',
+        'make:interface',
+        'make:job',
+        'make:listener',
+        'make:mail',
+        'make:model',
+        'make:notification',
+        'make:observer',
+        'make:policy',
+        'make:provider',
+        'make:request',
+        'make:resource',
+        'make:rule',
+        'make:scope',
+        'make:test',
+        'make:trait',
+        'make:view',
+        'make:controller',
+        'make:middleware',
+        'make:factory',
+        'make:seeder',
+        'make:migration',
+    ];
+
+    foreach ($makeCommands as $command) {
+        Artisan::call($command, ['--help']);
+        expect(Artisan::output())->toContain('--module');
+    }
+});
